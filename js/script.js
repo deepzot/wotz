fbacff5f61821570110322689fbeb17a78798c7c
David Kirkby <dkirkby@uci.edu>
2012-04-24 22:25:54 -0700

    Implement reset and jump buttons

--- js/script.js
+++ js/script.js
@@ -2,18 +2,38 @@
 
 */
 
-var theDate,dateOffset;
+// simulation start
+var startDate = new Date('January 1, 2010 9:25:00 AM PST');
+
+// offset between current time and simulation time in millisecs
+var dateOffset;
+
+// interval timer
+var timer = null;
 
 $(document).ready(function(){
-  // Parse our start date.
-  theDate = new Date($('#theDate').text());
-  // Calculate the offset between current time and our start date.
-  dateOffset = new Date() - theDate;  
-  // Start a 1Hz interval timer.
-  setInterval(function(){
-    var now = new Date();
-    // Calculate and display the offset time.
-    var then = new Date(now.getTime() - dateOffset);
-    $('#theDate').text(then.toLocaleString());
-  },1000 /* in millisecs */ );
+
+  // Install and invoke a reset action.
+  $('#resetButton').click(function() {
+    // Display the simulation start date.
+    $('#theDate').text(startDate.toLocaleString());
+    // Calculate the offset between current time and our start date.
+    dateOffset = new Date() - startDate;
+    // Clear any interval timer that is already running.
+    if(timer) clearInterval(timer);
+    // Start a new 1Hz interval timer.
+    timer = setInterval(function(){
+      var now = new Date();
+      // Calculate and display the offset time.
+      var then = new Date(now.getTime() - dateOffset);
+      $('#theDate').text(then.toLocaleString());
+    },1000 /* in millisecs */ );    
+  }).click();
+
+  // Install a jump action.
+  $('#jumpButton').click(function() {
+    var jumpOffset = Math.random()*86400e3;
+    dateOffset = dateOffset - jumpOffset;
+  });
+
 });
