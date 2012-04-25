87c92259461ef2fee00b9010ce93cc922c13e782
David Kirkby <dkirkby@uci.edu>
2012-04-24 21:48:22 -0700

    Implement an offset real-time clock

--- js/script.js
+++ js/script.js
@@ -2,7 +2,18 @@
 
 */
 
+var theDate,dateOffset;
 
-
-
-
+$(document).ready(function(){
+  // Parse our start date.
+  theDate = new Date($('#theDate').text());
+  // Calculate the offset between current time and our start date.
+  dateOffset = new Date() - theDate;  
+  // Start a 1Hz interval timer.
+  setInterval(function(){
+    var now = new Date();
+    // Calculate and display the offset time.
+    var then = new Date(now.getTime() - dateOffset);
+    $('#theDate').text(then.toLocaleString());
+  },1000 /* in millisecs */ );
+});
