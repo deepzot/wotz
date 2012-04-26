32a4c8da836fa3885770b29c40aeab72d663cf6b
David Kirkby <dkirkby@uci.edu>
2012-04-26 10:00:01 -0700

    polish up (need to create Demo class soon)

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@
 // Represents a GreenButton IntervalReading.
 function IntervalReading(xml) {
   // Convert the xml start value to a javascript date.
-  this.start = new Date($(xml).find('start').text()*1000);
+  var utc = new Date($(xml).find('start').text()*1000);
+  this.start = new Date(utc.getTime() + utc.getTimezoneOffset()*60000);
+  this.duration = Number($(xml).find('duration').text());
   this.value = Number($(xml).find('value').text());
 }
 
@@ -19,14 +21,21 @@ function GreenButtonData(xml) {
     // Use the 'self' alias since 'this' is now hidden
     self.readings.push(new IntervalReading(this));
   });
-  this.startDate = this.readings[0].start;
-  log('startDate',this.startDate);
+  this.firstDate = this.readings[0].start;
+  $('#firstDate').text(this.firstDate.toLocaleString());
+  this.nReadings = this.readings.length;
+  $('#nReadings').text(this.nReadings);
+  this.lastDate = this.readings[this.nReadings-1].start;
+  $('#lastDate').text(this.lastDate.toLocaleString());
+  this.startDate = this.readings[this.nReadings/10].start;
+  $('#startDate').text(this.startDate.toLocaleString());
+  this.current = 0;
 }
 
 var theData = null;
 
-// simulation start
-var startDate = new Date('January 1, 2010 9:25:00 AM PST');
+// simulation time
+var demoDate = null;
 
 // offset between current time and simulation time in millisecs
 var dateOffset;
@@ -34,15 +43,34 @@ var dateOffset;
 // interval timer
 var timer = null;
 
-function update() {
+function timerUpdate() {
   var now = new Date();
   // Calculate and display the offset time.
-  var then = new Date(now.getTime() - dateOffset);
-  $('#theDate').text(then.toLocaleString());  
+  demoDate = new Date(now.getTime() - dateOffset);
+  $('#theDate').text(demoDate.toLocaleString());  
+}
+
+function demoUpdate() {
+  $('#rawData').empty();
+  var current = theData.current;
+  while(current < theData.nReadings && theData.readings[current].start < demoDate) {
+    current++;
+  }
+  theData.current = current;
+  for(var index = current-10; index <= current; index++) {
+    var reading = theData.readings[index];
+    $('#rawData').append('<div>' + reading.value + ' at ' +
+      reading.start.toLocaleString() + '</div>');
+  }
 }
 
 $(document).ready(function(){
 
+  $('#welcome').show();
+  $('#intro').hide();
+  $('#demo').hide();
+
+  // Implement the welcome handler.
   $('#loadData').submit(function() {
     var target = $('input[name=url]').val();
     log('loading',target);
@@ -55,38 +83,47 @@ $(document).ready(function(){
         log('loaded');
         theData = new GreenButtonData(xml);
         log('parsed');
+        $('#welcome').hide();
+        $('#intro').show();
       }
     });    
     return false; // prevent further form submission
   });
 
-  // Load a GreenButton data file to use.
-  /*
-  */
+  // Implement the intro handler.
+  $('#startDemo').click(function() {
+    log('starting');
+    $('#intro').hide();
+    $('#demo').show();
+    $('#resetButton').click();
+  });
 
-  // Install and invoke a reset action.
+  // Implement the reset handler.
   $('#resetButton').click(function() {
     // Calculate the offset between current time and our start date.
-    dateOffset = new Date() - startDate;
+    dateOffset = new Date() - theData.startDate;
     // Clear any interval timer that is already running.
     if(timer) clearInterval(timer);
     // Update now for immediate feedback.
-    update();
+    timerUpdate();
     // Start a new 1Hz interval timer.
-    timer = setInterval(update,1000 /* in millisecs */ );    
-  }).click();
+    timer = setInterval(timerUpdate,1000 /* in millisecs */ );
+    theData.current = 0;
+    demoUpdate();
+  });
 
-  // Install a jump action.
+  // Implement the jump handler.
   $('#jumpButton').click(function() {
     // Calculate a random jump offset in millisecs.
-    var jumpOffset = Math.random()*86400e3;
+    var jumpOffset = (5 + 4*Math.random())*86400e3;
     dateOffset = dateOffset - jumpOffset;
     // Clear any interval timer that is already running.
     if(timer) clearInterval(timer);
     // Update now for immediate feedback.
-    update();
+    timerUpdate();
     // Start a new 1Hz interval timer.
-    timer = setInterval(update,1000 /* in millisecs */ );    
+    timer = setInterval(timerUpdate,1000 /* in millisecs */ );
+    demoUpdate();
   });
 
 });
