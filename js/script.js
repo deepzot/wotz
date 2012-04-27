b960bcbc784fefe872d4df10ba82133ae821c444
David Kirkby <dkirkby@uci.edu>
2012-04-27 12:27:07 -0700

    move GreenButton data stuff into its own file

--- js/script.js
+++ js/script.js
@@ -2,36 +2,6 @@
 
 */
 
-// Represents a GreenButton IntervalReading.
-function IntervalReading(xml) {
-  // Convert the xml start value to a javascript date.
-  var utc = new Date($(xml).find('start').text()*1000);
-  this.start = new Date(utc.getTime() + utc.getTimezoneOffset()*60000);
-  this.duration = Number($(xml).find('duration').text());
-  this.value = Number($(xml).find('value').text());
-}
-
-// Represents a GreenButton data file.
-function GreenButtonData(xml) {
-  var self = this;
-  this.readings = new Array();
-  // Save all IntervalReading elements to memory
-  // TODO: check for gaps and how daylight savings is handled
-  $(xml).find('IntervalReading').each(function() {
-    // Use the 'self' alias since 'this' is now hidden
-    self.readings.push(new IntervalReading(this));
-  });
-  this.firstDate = this.readings[0].start;
-  $('.firstDate').text(this.firstDate.toLocaleString());
-  this.nReadings = this.readings.length;
-  $('.nReadings').text(this.nReadings);
-  this.lastDate = this.readings[this.nReadings-1].start;
-  $('.lastDate').text(this.lastDate.toLocaleString());
-  this.startDate = this.readings[Math.floor(this.nReadings/10)].start;
-  $('.startDate').text(this.startDate.toLocaleString());
-  this.current = 0;
-}
-
 var theData = null;
 
 // simulation time
