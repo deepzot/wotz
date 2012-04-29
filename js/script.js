55af7c60442caf33bc02870e3f757eee8a37f9f4
David Kirkby <dkirkby@uci.edu>
2012-04-29 09:26:29 -0700

    move js loading after content and include demo code (not running yet)

--- js/script.js
+++ js/script.js
@@ -4,4 +4,4 @@ demo.modules.push(new GameModule());
 demo.modules.push(new ExploreModule());
 demo.modules.push(new ChallengeModule());
 
-$(document).ready(function(){ demo.start(); });
+//$(document).ready(function(){ demo.start(); });
