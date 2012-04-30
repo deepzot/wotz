8d56518556b0d4018b07e3857fb185db713c3664
David Kirkby <dkirkby@uci.edu>
2012-04-30 14:06:35 -0700

    fix svg graphics vertical overflow problem

--- js/challenge.js
+++ js/challenge.js
@@ -8,30 +8,7 @@ ChallengeModule.prototype.start = function(data) {
 }
 
 ChallengeModule.prototype.update = function(data,container) {
-  log('challenge update',data.current,container.width(),container.height());
-  // Grab the most recent 48 readings.
-  var recent = data.getRecent(48);
-  // Draw a graph of these readings.
-  $('#moduleContent').empty();
-  var graph = d3.select('#moduleContent').append("svg")
-    .attr("id", "challengeGraph")
-    .attr("width", 600)
-    .attr("height", 400);
-  var x = d3.scale.linear()
-    .domain([recent[0].start.getTime(),recent[recent.length-1].start.getTime()+3600000])
-    .range([0,600])
-  var y = d3.scale.linear()
-    .domain([2000,0])
-    .range([0,400]);
-  graph.selectAll("rect")
-    .data(recent)
-    .enter().append("rect")
-      .attr("x", function(d,i) { return x(d.start.getTime()) })
-      .attr("y", function(d,i) { return y(d.value) })
-      .attr("height", function(d,i) { return 400-y(d.value) })
-      .attr("width", function(d,i) {
-        return x(d.start.getTime()+d.duration*1000)-x(d.start.getTime())
-      });
+log('challenge update',data.current);
 }
 
 ChallengeModule.prototype.end = function() { }
--- js/explore.js
+++ js/explore.js
@@ -8,7 +8,29 @@ ExploreModule.prototype.start = function(data) {
 }
 
 ExploreModule.prototype.update = function(data,container) {
-  log('explore update',data.current);
+  // Grab the most recent 48 readings.
+  var recent = data.getRecent(48);
+  // Draw a graph of these readings.
+  $('#moduleContent').empty();
+  var graph = d3.select('#moduleContent').append("svg")
+    .attr('class','graphics')
+    .attr('id', 'exploreGraph');
+  var width = $('#exploreGraph').width(), height = $('#exploreGraph').height();
+  var x = d3.scale.linear()
+    .domain([recent[0].start.getTime(),recent[recent.length-1].start.getTime()+3600000])
+    .range([0,width-1])
+  var y = d3.scale.linear()
+    .domain([2000,0])
+    .range([0,height-1]);
+  graph.selectAll("rect")
+    .data(recent)
+    .enter().append("rect")
+      .attr("x", function(d,i) { return x(d.start.getTime()) })
+      .attr("y", function(d,i) { return y(d.value) })
+      .attr("height", function(d,i) { return height-y(d.value) })
+      .attr("width", function(d,i) {
+        return x(d.start.getTime()+d.duration*1000)-x(d.start.getTime())
+      });
 }
 
 ExploreModule.prototype.end = function() { }
