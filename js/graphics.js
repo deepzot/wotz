4cb413c0b1188caa53b77d7f0943f4b67e4b057b
David Kirkby <dkirkby@uci.edu>
2012-05-09 15:20:25 -0700

    Add new javascript Graphics class (not used anywhere yet).

---
+++ js/graphics.js
@@ -0,0 +1,18 @@
+// Creates a new SVG graphics element that fills the specified container.
+// The new SVG element will have class 'graphics' and the specified id name.
+// Any previous contents of the container will be discarded.
+function Graphics(container,name) {
+  // Remember our container in case we need to redraw under our own control.
+  this.container = $(container);
+  // Draw a graph of these readings.
+  this.container.empty();
+  this.graph = d3.select(this.container.get(0)).append("svg:svg")
+    .attr('class','graphics')
+    .attr('id', name);
+  // Fill our container.
+  this.width = this.container.width();
+  this.height = this.container.height();
+  this.graph.attr('width',width).attr('height',height);
+  // Create an empty SVG definitions section.
+  var this.defs = graph.append('svg:defs');
+}
