01ba9ac8db825bcbf5e8c41430ab18ba4172cb1f
David Kirkby <dkirkby@uci.edu>
2012-05-08 17:49:38 -0700

    Try moving inline facebook SDK javascript into Facebook ctor.

--- js/social.js
+++ js/social.js
@@ -1,5 +1,32 @@
 function Facebook() {
+  self = this;
+  
   this.loggedIn = false;
+  
+  <!-- Load the facebook SDK (http://developers.facebook.com/docs/guides/mobile/web/#sdk) -->
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '420170771335687', // App ID
+      channelUrl : '//darkmatter.ps.uci.edu/gbtest/channel.html', // Channel File
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
+    FB.Event.subscribe('auth.statusChange', function(response) {
+      if (response.authResponse) {
+        log('facebookStatusChange',response);
+        self.loggedIn = true;
+      }
+    });
+  };
+  // Load the SDK Asynchronously
+  (function(d){
+     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     ref.parentNode.insertBefore(js, ref);
+   }(document));
 }
 
 Facebook.prototype.share = function(message) {
@@ -28,8 +55,6 @@ Facebook.prototype.handleStatusChange = function(response) {
     this.loggedIn = true;
   }
 }
-
-facebook = new Facebook();
 
 function share(activeModule) {
   var message = 'GBAPP is amazing.';
