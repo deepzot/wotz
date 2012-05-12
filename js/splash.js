490af8367eeab8d31d2356bd5a02e2c6f4940fc4
David Kirkby <dkirkby@uci.edu>
2012-05-12 07:49:06 -0700

    Add some animation effects to the splash text.

--- js/splash.js
+++ js/splash.js
@@ -16,22 +16,44 @@ Splash.prototype.update = function(container) {
   var group = graphics.graph.append('svg:g')
     .attr('id','splash')
     .attr('font-size','50');
+
   var line1 = group.append('svg:text')
     .attr('x',graphics.width/2)
-    .attr('y',graphics.height/2-85);
+    .attr('y',graphics.height/2-85)
+    .attr('opacity',0);
   line1.selectAll('tspan')
     .data(['wot','z',' a kilowatt?'])
     .enter().append('svg:tspan')
       .text(function(d) { return d; })
-      .attr('class',function(d) { return d=='z' ? 'gold':null; });
-  group.append('svg:text')
+      .attr('id',function(d) { return d=='z' ? 'goldz':null; });
+
+  var lines23 = group.append('svg:g')
+    .attr('opacity',0);
+  lines23.append('svg:text')
     .text('let your data')
     .attr('x',graphics.width/2)
     .attr('y',graphics.height/2+10);
-  group.append('svg:text')
+  lines23.append('svg:text')
     .text('do the talking...')
     .attr('x',graphics.width/2)
     .attr('y',graphics.height/2+85);
   
   var scale = graphics.centerVertically(group,0.8*graphics.width,0.8*graphics.height);
+  
+  line1.transition()
+    .delay(0) // ms
+    .duration(750) // ms
+    .attr('opacity',1);
+  
+  lines23.transition()
+    .delay(500) // ms
+    .duration(750) // ms
+    .attr('opacity',1);
+  
+  line1.select('#goldz')
+    .attr('fill','#4BB54E')
+    .transition()
+    .delay(1500) // ms
+    .duration(500) // ms
+    .attr('fill','#FFDE6C');
 }
