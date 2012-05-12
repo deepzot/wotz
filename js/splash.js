398cc98e08fd07235bd5cfc2a2fb78084e85b197
David Kirkby <dkirkby@uci.edu>
2012-05-12 07:32:25 -0700

    Rework splash module to generate group contents directly.

--- js/splash.js
+++ js/splash.js
@@ -7,10 +7,31 @@ Splash.prototype.start = function(data,settings) { }
 Splash.prototype.end = function() { }
 
 Splash.prototype.update = function(container) {
-  this.graphics = new Graphics(container,'splashGraph');
-  this.graphics.graph.append('svg:rect')
+  var graphics = new Graphics(container,'splashGraph');
+  graphics.graph.append('svg:rect')
     .attr('fill','white')
-    .attr('width',this.graphics.width)
-    .attr('height',this.graphics.height);
-  this.graphics.showMessage(['wotz a kiloWatt?','let your data','do the talking...'],true);
+    .attr('width',graphics.width)
+    .attr('height',graphics.height);
+  
+  var group = graphics.graph.append('svg:g')
+    .attr('id','splash')
+    .attr('font-size','50');
+  var line1 = group.append('svg:text')
+    .attr('x',graphics.width/2)
+    .attr('y',graphics.height/2-85);
+  line1.selectAll('tspan')
+    .data(['wot','z',' a kilowatt?'])
+    .enter().append('svg:tspan')
+      .text(function(d) { return d; })
+      .attr('class',function(d) { return d=='z' ? 'gold':null; });
+  group.append('svg:text')
+    .text('let your data')
+    .attr('x',graphics.width/2)
+    .attr('y',graphics.height/2+10);
+  group.append('svg:text')
+    .text('do the talking...')
+    .attr('x',graphics.width/2)
+    .attr('y',graphics.height/2+85);
+  
+  var scale = graphics.centerVertically(group,0.8*graphics.width,0.8*graphics.height);
 }
