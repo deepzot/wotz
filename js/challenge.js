3dc5b943c0e16a8dfb73b7eb28d9923f635c439d
David Kirkby <dkirkby@uci.edu>
2012-05-12 14:59:41 -0700

    Use new Graphics library in challenge module.

--- js/challenge.js
+++ js/challenge.js
@@ -23,69 +23,55 @@ ChallengeModule.prototype.start = function(data) {
 
 ChallengeModule.prototype.update = function(container) {
   var self = this;
-  // Remember our container in case we need to redraw under our own control.
-  this.container = container;
-  // Initialize SVG
-  container.empty();
-  var graph = d3.select('#moduleContent').append("svg:svg")
-    .attr('class','graphics')
-    .attr('id', 'challengeGraph');
-  var width = $('#moduleContent').width(), height = $('#moduleContent').height();
-  graph.attr('width',width).attr('height',height);
-  // Calculate a nominal scaling unit for labels.
-  var emUnit = $('#challengeGraph').css('font-size');
-  if(emUnit.slice(-2) == 'px') {
-    emUnit = parseFloat(emUnit);
-  }
-  else {
-    emUnit = 10;
-  }
+  // Create a new SVG graphics element that fills our container.
+  var graphics = new Graphics(container,'challengeGraph');
+  this.graphics = graphics;
   // Draw a clock face.
-  var radius = 0.4*Math.min(height,width);
-  graph.append('svg:circle')
+  var radius = 0.4*Math.min(graphics.height,graphics.width);
+  graphics.graph.append('svg:circle')
     .attr('id','clockCircle')
-    .attr('cx',width/2)
-    .attr('cy',height/2)
+    .attr('cx',graphics.width/2)
+    .attr('cy',graphics.height/2)
     .attr('r',radius/2);
-  graph.selectAll('text.clockLabel')
+  graphics.graph.selectAll('text.clockLabel')
     .data([12,3,6,9])
     .enter().append('svg:text')
       .attr('class','clockLabel')
       .text(function(d) { return d; })
-      .attr('x',width/2)
-      .attr('y',height/2)
+      .attr('x',graphics.width/2)
+      .attr('y',graphics.height/2)
       .attr('transform',function(d) {
-        return 'rotate('+(30*d)+','+width/2+','+height/2+') translate(0,'+(-radius/2+2*emUnit)+')';
+        return 'rotate('+(30*d)+','+graphics.width/2+','+graphics.height/2+') translate(0,'+(-radius/2+2*graphics.fontSize)+')';
       });
   // Start real-time clock hands going.
-  graph.selectAll('line.clockHand')
+  graphics.graph.selectAll('line.clockHand')
     .data([0,0,0])
     .enter().append('svg:line')
       .attr('class','clockHand')
       .attr('opacity',0)
       .attr('id',function(d,i) { return 'clockHand'+i; })
-      .attr('x1',width/2)
-      .attr('y1',height/2+height/50)
-      .attr('x2',width/2)
-      .attr('y2',function(d,i) { return height/2 - ((i==0)? 0.35*radius : 0.45*radius); });
+      .attr('x1',graphics.width/2)
+      .attr('y1',graphics.height/2+graphics.height/50)
+      .attr('x2',graphics.width/2)
+      .attr('y2',function(d,i) { return graphics.height/2 - ((i==0)? 0.35*radius : 0.45*radius); });
   if(this.timer) clearInterval(this.timer);
   this.timer = setInterval(function() {
     var now = new Date();
     // Only hour hand moves continuously.
     var mins = now.getMinutes();
     var hands = [ (now.getHours()%12 + mins/60)/12, mins/60, now.getSeconds()/60 ];
-    graph.selectAll('line.clockHand').data(hands)
+    graphics.graph.selectAll('line.clockHand').data(hands)
       .attr('opacity',1)
-      .attr('transform',function(d) { return 'rotate('+360*d+','+width/2+','+height/2+')'});
+      .attr('transform',function(d) { return 'rotate('+360*d+','+graphics.width/2+','+graphics.height/2+')'});
   },1000);
   // Draw average hourly usage around the clock face.
-  var gap = emUnit/2;
+  var gap = graphics.fontSize/2;
   var hourlyArc = d3.svg.arc()
-    .innerRadius(radius/2+emUnit/2)
+    .innerRadius(radius/2+graphics.fontSize/2)
     .outerRadius(function(d) { return radius/2+gap + (radius/2-gap)*d/self.maxHourly; })
     .startAngle(function(d,i) { return (i+self.hourOrigin)*Math.PI/6; })
     .endAngle(function(d,i) { return (i+1+self.hourOrigin)*Math.PI/6; });
-  var hourlyDataG = graph.append('svg:g');
+  var hourlyDataG = graphics.graph.append('svg:g');
   hourlyDataG.selectAll('path.hourlyArc')
     .data(this.hourlyData)
     .enter()
@@ -93,7 +79,7 @@ ChallengeModule.prototype.update = function(container) {
       .attr('class','hourlyArc')
       .attr('opacity',function(d,i) { return 1-i/12; })
       .attr('d',hourlyArc);
-  hourlyDataG.attr('transform','translate('+width/2+','+height/2+')');
+  hourlyDataG.attr('transform','translate('+graphics.width/2+','+graphics.height/2+')');
 }
 
 ChallengeModule.prototype.end = function() { }
